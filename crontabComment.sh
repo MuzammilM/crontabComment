b88412b4f0ca33402e05ef082f9454fa141554a8
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-02-23 16:13:05 +0530

    Added crontabComment.sh

---
+++ crontabComment.sh
@@ -0,0 +1,14 @@
+#Author : MuzammilM
+#Comments script passed as an argument from crontab
+COLOR='\033[0;31m'
+reset=`tput sgr0`
+
+if [ $# -lt 1 ]
+then
+        echo -e "${COLOR}No arguments passed."
+        echo -e "${COLOR}Comments a script from crontab"
+        echo -e "${COLOR}Execution : crontabComment {scriptName.sh}${reset}"
+        exit
+fi
+crontab -l > cronoutput.txt && sed  '/'$1'/s/^/#/' cronoutput.txt > _c  &&  mv -f _c cronoutput.txt && crontab cronoutput.txt && rm cronoutput.txt
+crontab -l
